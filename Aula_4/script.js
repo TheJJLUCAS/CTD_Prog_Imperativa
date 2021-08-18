ede51420df5f3b4bc4b90e830b629f85597f58d4
unknown <joaolucasso@gmail.com>
2021-08-18 19:27:25 -0300

    Update_01

---
+++ Aula_4/script.js
@@ -0,0 +1,28 @@
+
+//Comentário
+
+/*
+Comentário de multiplas linhas
+Utilizada principalmente em documentações de código
+*/
+
+/*
+    Alerts
+    Documentação: https://developer.mozilla.org/pt-BR/docs/Web/API/Window/alert
+
+    alert(texto|valor);
+
+    Prompts
+    Documentação: https://developer.mozilla.org/pt-BR/docs/Web/API/Window/prompt
+
+    prompt(texto, valor);
+
+    Confirm
+    Documentação: https://developer.mozilla.org/pt-BR/docs/Web/API/Window/confirm
+
+    confirm(mensagem);
+*/
+
+var saudacao = 'Olá'
+var nome = prompt('Qual é o seu nome?');
+alert(saudacao + ' ' + nome);
---
+++ Aula_4/script2.js
@@ -0,0 +1,7 @@
+// Comentário de única linha
+/*
+Comenários de mais de uma linha
+*/
+
+var saudacao ="Olá";
+var idade = 15
