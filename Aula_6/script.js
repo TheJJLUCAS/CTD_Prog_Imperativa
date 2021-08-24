667ca4cc213dea218cdd89a5aeeb62ee451c0d5b
unknown <joaolucasso@gmail.com>
2021-08-24 19:49:21 -0300

    Update_02

---
+++ Aula_6/script.js
@@ -0,0 +1,15 @@
+
+// Construir a função / Declarar
+function cumprimentar () {
+    // O código que será reutilizado será inserido dentro de uma função
+    alert('Olá');
+}
+
+// Função com parâmetro
+function saudar(saudacao) {
+    alert(saudacao);
+}
+
+// Invocar a função (Execução)
+cumprimentar();
+saudar('Olá');
